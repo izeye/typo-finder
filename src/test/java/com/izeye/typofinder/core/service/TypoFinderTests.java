376fbb11709db0e29686e11bc30f553b81b0c8de
Johnny Lim <izeye@naver.com>
2015-10-28 10:18:54 +0900

    Update dictionaries

--- src/test/java/com/izeye/typofinder/core/service/TypoFinderTests.java
+++ src/test/java/com/izeye/typofinder/core/service/TypoFinderTests.java
@@ -26,10 +26,11 @@ public class TypoFinderTests {
 	@Test
 	public void testFindTypos() throws IOException {
 //		String filename = "src/test/resources/samples/test.txt";
-		String filename = "/Users/izeye/IdeaProjects/spring-boot/spring-boot-docs/src/main/asciidoc/documentation-overview.adoc";
+//		String filename = "/Users/izeye/IdeaProjects/spring-boot/spring-boot-docs/src/main/asciidoc/documentation-overview.adoc";
 //		String filename = "/Users/izeye/IdeaProjects/spring-boot/spring-boot-docs/src/main/asciidoc/getting-started.adoc";
-//		String filename = "/Users/izeye/IdeaProjects/spring-boot/spring-boot-docs/src/main/asciidoc/production-ready-features.adoc";
 //		String filename = "/Users/izeye/IdeaProjects/spring-boot/spring-boot-docs/src/main/asciidoc/using-spring-boot.adoc";
+		String filename = "/Users/izeye/IdeaProjects/spring-boot/spring-boot-docs/src/main/asciidoc/spring-boot-features.adoc";
+//		String filename = "/Users/izeye/IdeaProjects/spring-boot/spring-boot-docs/src/main/asciidoc/production-ready-features.adoc";
 		
 		String text = IOUtils.toString(new FileReader(filename));
 		Set<WordToken> typos = this.typoFinder.findTypos(text);
