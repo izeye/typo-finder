d0c9d7a2b3bc28fdf9978485b7b6cc17c4b0b359
Johnny Lim <izeye@naver.com>
2015-11-12 11:44:53 +0900

    Update dictionaries

--- src/main/java/com/izeye/typofinder/core/config/DictionaryConfig.java
+++ src/main/java/com/izeye/typofinder/core/config/DictionaryConfig.java
@@ -18,17 +18,20 @@ public class DictionaryConfig {
 	private static final String RESOURCE_LOCATION_ANT_TERMS = "classpath:dictionary/ant_terms.txt";
 	private static final String RESOURCE_LOCATION_ASCIIDOCTOR_TERMS = "classpath:dictionary/asciidoctor_terms.txt";
 	private static final String RESOURCE_LOCATION_BASH_TERMS = "classpath:dictionary/bash_terms.txt";
+	private static final String RESOURCE_LOCATION_DOMAIN_TERMS = "classpath:dictionary/domain_terms.txt";
 	private static final String RESOURCE_LOCATION_GRADLE_TERMS = "classpath:dictionary/gradle_terms.txt";
 	private static final String RESOURCE_LOCATION_GROOVY_TERMS = "classpath:dictionary/groovy_terms.txt";
 	private static final String RESOURCE_LOCATION_HEROKU_TERMS = "classpath:dictionary/heroku_terms.txt";
 	private static final String RESOURCE_LOCATION_HIBERNATE_TERMS = "classpath:dictionary/hibernate_terms.txt";
 	private static final String RESOURCE_LOCATION_HTML_TERMS = "classpath:dictionary/html_terms.txt";
+	private static final String RESOURCE_LOCATION_HTTP_TERMS = "classpath:dictionary/http_terms.txt";
 	private static final String RESOURCE_LOCATION_JAVA_TERMS = "classpath:dictionary/java_terms.txt";
 	private static final String RESOURCE_LOCATION_JWT_TERMS = "classpath:dictionary/jwt_terms.txt";
 	private static final String RESOURCE_LOCATION_LINUX_TERMS = "classpath:dictionary/linux_terms.txt";
 	private static final String RESOURCE_LOCATION_MAVEN_TERMS = "classpath:dictionary/maven_terms.txt";
 	private static final String RESOURCE_LOCATION_SPRING_TERMS = "classpath:dictionary/spring_terms.txt";
 	private static final String RESOURCE_LOCATION_SPRING_BOOT_TERMS = "classpath:dictionary/spring_boot_terms.txt";
+	private static final String RESOURCE_LOCATION_TOMCAT_TERMS = "classpath:dictionary/tomcat_terms.txt";
 	
 	@Bean
 	public Dictionary englishWordsDictionary() {
@@ -42,17 +45,20 @@ public class DictionaryConfig {
 				RESOURCE_LOCATION_ANT_TERMS,
 				RESOURCE_LOCATION_ASCIIDOCTOR_TERMS,
 				RESOURCE_LOCATION_BASH_TERMS,
+				RESOURCE_LOCATION_DOMAIN_TERMS,
 				RESOURCE_LOCATION_GRADLE_TERMS,
 				RESOURCE_LOCATION_GROOVY_TERMS,
 				RESOURCE_LOCATION_HEROKU_TERMS,
 				RESOURCE_LOCATION_HIBERNATE_TERMS,
 				RESOURCE_LOCATION_HTML_TERMS,
+				RESOURCE_LOCATION_HTTP_TERMS,
 				RESOURCE_LOCATION_JAVA_TERMS,
 				RESOURCE_LOCATION_JWT_TERMS,
 				RESOURCE_LOCATION_LINUX_TERMS,
 				RESOURCE_LOCATION_MAVEN_TERMS,
 				RESOURCE_LOCATION_SPRING_TERMS,
-				RESOURCE_LOCATION_SPRING_BOOT_TERMS);
+				RESOURCE_LOCATION_SPRING_BOOT_TERMS,
+				RESOURCE_LOCATION_TOMCAT_TERMS);
 	}
 	
 }
