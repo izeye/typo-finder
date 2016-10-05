7147b3078a892849cd9f05b656f20a660591e8cc
Johnny Lim <izeye@naver.com>
2016-10-05 16:57:18 +0900

    Update dictionaries

--- src/main/java/com/izeye/typofinder/core/repository/DefaultDictionary.java
+++ src/main/java/com/izeye/typofinder/core/repository/DefaultDictionary.java
@@ -8,7 +8,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by izeye on 15. 10. 23..
@@ -21,7 +23,10 @@ public class DefaultDictionary implements Dictionary {
 		for (String resourceLocation : resourceLocations) {
 			try {
 				File file = ResourceUtils.getFile(resourceLocation);
-				this.words.addAll(IOUtils.readLines(new FileReader(file)));
+				List<String> lines = IOUtils.readLines(new FileReader(file));
+				List<String> filtered = lines.stream().filter(line -> !line.startsWith("#"))
+						.collect(Collectors.toList());
+				this.words.addAll(filtered);
 			} catch (FileNotFoundException ex) {
 				throw new RuntimeException(ex);
 			} catch (IOException ex) {
